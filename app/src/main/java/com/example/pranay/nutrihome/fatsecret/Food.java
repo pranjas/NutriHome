7deba4167b9da06ede79ca04b3968093999b0e65
Pranay Kr. Srivastava <pranjas@gmail.com>
2016-09-17 17:55:29 +0530

    add more information about food item
    This patch
    1) adds nutritional information about each food item as
         per it's measurement.

--- app/src/main/java/com/example/pranay/nutrihome/fatsecret/Food.java
+++ app/src/main/java/com/example/pranay/nutrihome/fatsecret/Food.java
@@ -23,6 +23,7 @@ import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 /**
  * Created by pranay on 17/9/16.
@@ -59,13 +60,13 @@ public class Food {
 
             for (int i = 0; i < allFoods.length(); i++) {
                 JSONObject jsonItem = allFoods.getJSONObject(i);
-                FoodInfo fInfo = new FoodInfo();
-                        fInfo.food_id = jsonItem.getString("food_id");
-                        fInfo.food_name = jsonItem.getString("food_name");
-                        fInfo.food_type = jsonItem.getString("food_type");
-                        fInfo.brand_name = jsonItem.optString("brand_name");
-                        fInfo.food_description = jsonItem.getString("food_description");
-                        fInfo.food_url = jsonItem.getString("food_url");
+                FoodInfo fInfo = new FoodInfo(
+                        jsonItem.getString("food_id"),
+                        jsonItem.getString("food_name"),
+                        jsonItem.getString("food_type"),
+                        jsonItem.optString("brand_name"),
+                        jsonItem.getString("food_description"),
+                        jsonItem.getString("food_url"));
                 items.add(fInfo);
             }
 
@@ -86,6 +87,54 @@ public class Food {
                 food_description,
                 brand_name;
 
+        public FoodInfo(String food_id, String food_name, String food_type,
+                        String brand_name, String food_description, String food_url)
+        {
+            this.brand_name = brand_name;
+            this.food_description = food_description;
+            this.food_url = food_url;
+            this.food_name = food_name;
+            this.food_type = food_type;
+            this.food_id = food_id;
+            setCalories();
+        }
+
+        private void setCalories()
+        {
+            StringTokenizer st = new StringTokenizer(food_description, "|");
+            while (st.hasMoreElements()) {
+                String next = st.nextElement().toString();
+
+                if(next.toLowerCase().contains("fat"))
+                    fat = next.split(":")[1].trim();
+
+                else if (next.toLowerCase().contains("carbs"))
+                    carbohydrates = next.split(":")[1].trim();
+
+                else if (next.toLowerCase().contains("protein"))
+                    protein = next.split(":")[1].trim();
+
+                else if (next.toLowerCase().contains("calories")) {
+
+                    StringTokenizer forMeasurement = new StringTokenizer(next,"-");
+                    while (forMeasurement.hasMoreElements()) {
+                        String val = forMeasurement.nextElement().toString();
+                        if (val.toLowerCase().contains("calories"))
+                            calories = val.split(":")[1].trim();
+                        else
+                            measurement = val.trim();
+                    }
+                }
+            }
+        }
+        protected String
+                    protein,
+                    carbohydrates,
+                    calories,
+                    fat;
+
+        protected String measurement;
+
         public boolean isGeneric()
         {
             return food_type.toLowerCase().equals("generic");
@@ -104,6 +153,10 @@ public class Food {
             sb.append("Type: " + food_type +"\n");
             sb.append("URL: " + food_url + "\n");
             sb.append("Description: " + food_description + "\n");
+            sb.append("Carbs: "+ carbohydrates + "\n");
+            sb.append("Protein: " + protein + "\n");
+            sb.append("Fat: " + fat + "\n");
+            sb.append("Measurement: " + measurement + "\n");
             sb.append("Brand: " + brand_name + "\n===\n");
 
             return sb.toString();
